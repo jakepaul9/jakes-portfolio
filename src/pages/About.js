8b37589ef23a3dd6951e78f8acfb9b4ee81968a6
Jake Grether <jgrether@Jakes-MBP.hsd1.ut.comcast.net>
2021-08-27 14:46:57 -0600

    added about content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,52 @@
 import React from 'react'
 
+import me from '../assets/images/me.jpg'
+import spartan from '../assets/images/spartan.jpg'
+import './Page.css'
+import './About.css'
+
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className='container-dark'>
+      <div className='header-dark'></div>
+      <div className='content-dark'>
+        <div className='page-title-dark'>me</div>
+
+        <div className='page-description-dark'>
+          <img className='me-img' src={me} alt='me' />
+          Welcome to my online portfolio! My name is{' '}
+          <span className='emph'>Jake Grether</span>, a child of the 90's born
+          and raised in an old gold-mining town in southern Oregon called Gold
+          Hill. After graduating highschool in 2009 I left my hometown to serve
+          a two year mission in Orlando Florida for my church. I returned home,
+          moved to Utah and 6 months later{' '}
+          <span className='emph'> married</span> my girlfriend of over two
+          years. <br />
+          <br />I started my college education at SLCC, then later transfered to
+          <span className='emph'> UVU</span> where I graduated in December 2020
+          with my bachelors degree in
+          <span className='emph'> Computer Science</span>. I emphasized in{' '}
+          <span className='emph'>full-stack</span> web development and have come
+          to really enjoy creating projects using the{' '}
+          <span className='emph'>MERN</span> stack. I also grew an appreciation
+          and learned to enjoy coding in <span className='emph'>C++</span>.{' '}
+          <br /> <br />
+          <img className='me-img-left' src={spartan} alt='spartan' />
+          My true passion is music writing and recording. I also enjoy working
+          on full-stack projects on the side including{' '}
+          <span className='emph'> e-commerce</span> development, and am
+          currently teaching myself <span className='emph'>React Native</span>{' '}
+          and <span className='emph'> electronics/robotics </span>
+          with my <span className='emph'>Raspberry Pi</span>. My wife and I
+          enjoy doing outdoor activities together including hiking, and the
+          occasional Spartan Race.
+          <br />
+          <br />
+          I've included some pages here showcasing things about me you may be
+          interested in, especially if you're considering me as a new member of
+          your team. Enjoy!
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Music.js
+++ src/pages/Music.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactAudioPlayer from 'react-audio-player'
 
 import './Music.css'
+import './Page.css'
 
 import song1 from '../assets/songs/A Lifetime With You.m4a'
 import song2 from '../assets/songs/Clean Slate.m4a'
@@ -20,7 +21,7 @@ const Music = () => {
     <div className='container-dark'>
       <div className='header-dark'></div>
       <div className='content-dark'>
-        <div className='page-title-dark'>home recordings</div>
+        <div className='page-title-dark'>music</div>
         <div className='page-description-dark'>
           I've always loved music, but it wasn't until middle school that I
           really got into "instrument-ing". I started out with the trumpet in my
@@ -29,7 +30,7 @@ const Music = () => {
           piano and bass to satisfy the demands of the songs I've written. I
           find musical inspiration through artists like Hank Williams Sr,
           Benjamin Todd, Pete Bernhard, Towns Van Zandt, Johnny Cash, Brian May,
-          Maynard Keenan, Rivers Quomo, Trent Resnor, and the Gallagher bothers
+          Maynard Keenan, Rivers Cuomo, Trent Resnor, and the Gallagher bothers
           to name a few. I mostly write songs about my life and things I've
           experienced. Although I plan to one day have all my songs recorded
           professionally, I have a small music recording setup at home. Here are
@@ -37,47 +38,102 @@ const Music = () => {
           <div className='songs'>
             <div className='song'>
               <div className='song-title'> a lifetime with you</div>
-              <ReactAudioPlayer src={song1} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song1}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> clean slate</div>
-              <ReactAudioPlayer src={song2} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song2}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> sleep deprivation</div>
-              <ReactAudioPlayer src={song3} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song3}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> auto pilot</div>
-              <ReactAudioPlayer src={song4} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song4}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> rain dance</div>
-              <ReactAudioPlayer src={song5} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song5}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> damage control</div>
-              <ReactAudioPlayer src={song6} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song6}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'>a moment of silence</div>
-              <ReactAudioPlayer src={song7} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song7}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> never let it die</div>
-              <ReactAudioPlayer src={song8} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song8}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> wordsmith</div>
-              <ReactAudioPlayer src={song9} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song9}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> dark clouds</div>
-              <ReactAudioPlayer src={song10} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song10}
+                autoPlay={false}
+                controls
+              />
             </div>
             <div className='song'>
               <div className='song-title'> simple song</div>
-              <ReactAudioPlayer src={song11} autoPlay={false} controls />
+              <ReactAudioPlayer
+                className='player'
+                src={song11}
+                autoPlay={false}
+                controls
+              />
             </div>
           </div>
         </div>
