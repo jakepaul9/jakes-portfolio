3455847a693f13efe0c043b172cc79ff5d7fe781
Jake Grether <jgrether@Jakes-MBP.hsd1.ut.comcast.net>
2021-12-23 13:45:37 -0700

    added content to code page

--- src/Projects.js
+++ src/Projects.js
@@ -1,19 +1,23 @@
 import cm from './assets/images/CM.png'
+
 let Projects = [
   {
     title: 'ClutterMine',
     image: `${cm}`,
-    description: `<span className='emph'>ClutterMine</span> is a mass garage sale
+    description: `ClutterMine is a mass garage sale
         business idea where people who want to get rid of, and maybe make a
         little money from items taking up room in their homes, but who lack
         the time/energy to go about posting ads, or even the means to take
         their "junk" to the dump, can put there items on consigment with
-        <span className='emph'>ClutterMine</span> who will do the rest.
+        ClutterMine who will do the rest.
         <br /><br />
-        <span className='emph'>ClutterMine</span> will take the "clutter" to
+        ClutterMine will take the "clutter" to
         thier storefront where these items will sit for sale. As soon as the
         items are sold, the customer will recieve the consigment percentage
-        that was agreed upon during the onboarding process.`,
+        that was agreed upon during the onboarding process.
+        <br /><br />
+        The system behind this idea is being built with Node on the backend, 
+        and React Native on the front end. The database is MySQL.`,
   },
 ]
 
